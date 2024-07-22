1833190bb8490a0f9851d730f45a43ad74503068
Vinicius Anderle <vinianderle00@gmail.com>
2024-07-22 11:20:10 -0300

    Ajuste css

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -3,18 +3,19 @@
         <a class="navbar-brand" href="{{ url('/') }}">MyApp</a>
 
         <div class="collapse navbar-collapse" id="navbarNav">
-            <ul class="navbar-nav ml-auto">
-                <a class="nav-link" href="{{ url('/hotels') }}">Hotels</a>
-                <a class="nav-link" href="{{ url('/rooms') }}">Rooms</a>
-                <a class="nav-link" href="{{ url('/customers') }}">Customers</a>
-                <a class="nav-link" href="{{ url('/reservations') }}">Reservations</a>
-            </ul>
-            {{ Auth::user()->name }} <span class="caret"></span>
+        
+            Usuário: {{ Auth::user()->name }} <span class="caret"></span>
 
         </div>
         <form action="{{ route('logout') }}" method="POST">
             @csrf
             <button id="logout" type="submit" class="btn-logout">Logout</button>
         </form>
+        <ul class="navbar-nav ml-auto">
+                <a class="nav-link" href="{{ url('/hotels') }}">Hotels</a>
+                <a class="nav-link" href="{{ url('/rooms') }}">Rooms</a>
+                <a class="nav-link" href="{{ url('/customers') }}">Customers</a>
+                <a class="nav-link" href="{{ url('/reservations') }}">Reservations</a>
+            </ul>
     </nav>
 </header>
