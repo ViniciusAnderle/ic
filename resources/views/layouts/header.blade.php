4007b81fa7132f4d38635e9515be6792c39a4654
Vinicius Anderle <vinianderle00@gmail.com>
2024-07-21 23:06:37 -0300

    Ajuste no header e no cadastro da senha

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -1,9 +1,7 @@
 <header>
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
         <a class="navbar-brand" href="{{ url('/') }}">MyApp</a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
+
         <div class="collapse navbar-collapse" id="navbarNav">
             <ul class="navbar-nav ml-auto">
                 <li class="nav-item">
