7cf13cbd448e2367fa698778e9c36b5c598a3a97
Vinicius Anderle <vinianderle00@gmail.com>
2024-09-24 15:24:56 -0300

    Sessions added

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -3,7 +3,7 @@
         'name' => 'laravel/laravel',
         'pretty_version' => 'dev-main',
         'version' => 'dev-main',
-        'reference' => '86af34736d4af76210c1107b57abe6f403e55e6c',
+        'reference' => '27822cde38ee122bbca77302b943ae87f7124e8b',
         'type' => 'project',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -382,7 +382,7 @@
         'laravel/laravel' => array(
             'pretty_version' => 'dev-main',
             'version' => 'dev-main',
-            'reference' => '86af34736d4af76210c1107b57abe6f403e55e6c',
+            'reference' => '27822cde38ee122bbca77302b943ae87f7124e8b',
             'type' => 'project',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
